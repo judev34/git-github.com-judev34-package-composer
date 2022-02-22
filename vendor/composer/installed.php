2ace40467e393856f183805763195c9dc4f255c1
judev34 <julien.coduert34@gmail.com>
2022-02-22 10:47:31 +0100

    secondcommit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '6c6c45828f689109aad69cd5c2defc7012f1a3cf',
         'name' => 'judev/package-composer',
         'dev' => true,
     ),
     'versions' => array(
         'judev/package-composer' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '6c6c45828f689109aad69cd5c2defc7012f1a3cf',
             'dev_requirement' => false,
         ),
     ),
